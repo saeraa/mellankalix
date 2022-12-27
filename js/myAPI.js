238b81da2217c29e19e80480ee08a1308eeb2cdc
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2022-12-27 10:02:17 +0100

    Changed the searchpath for the API

--- js/myAPI.js
+++ js/myAPI.js
@@ -1,14 +1,12 @@
-const myURL = 'http://127.0.0.1:5500/static/';
-
 export async function getMovies() {
-    const res = await fetch(myURL + 'database.json');
+    const res = await fetch('static/database.json');
     const data = await res.json();
 
     return data;
 };
 
 export async function getShowTimes() {
-    const res = await fetch(myURL + 'showtimes.json');
+    const res = await fetch('static/showtimes.json');
     const data = await res.json();
 
     return data;
